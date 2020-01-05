b09aa7040dbe6c62d116281c30e1acb48a37f975
Braighton <braighton.polack@gmail.com>
2020-01-05 01:36:44 -0800

    Problem 1 and Problem 1 alt solution (list comprehension) added

--- problem_1/problem_1_alt.py
+++ problem_1/problem_1_alt.py
@@ -1,4 +1,5 @@
 # Solution to Project Euler Problem 1
+# Alternate solution using Python List Comprehension - on average same runtime but less verbose
 
 #### Problem: 
 #### If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
@@ -8,23 +9,8 @@ import time
 
 start = time.time()
 
-multiples = set()
-def get_multiples(x, max_multiple):
-    i = 1
-    while (True):
-        multiple = x * i
-        if (multiple < max_multiple):
-            multiples.add(multiple)
-            i += 1
-        else:
-            break
-
-# Get multiples of 3 and 5 below 1000
-get_multiples(3, 1000)
-get_multiples(5, 1000)
-
-# Sum of all multiples in set
-total = sum(multiples)
+# Sum of all multiples of 3 or 5 below 1000
+total = sum([x for x in range(1000) if (x % 3 == 0 or x % 5 == 0)])
 
 elapsed = time.time() - start
 
